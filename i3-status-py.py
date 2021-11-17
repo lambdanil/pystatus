eb44eddd20cc3290cdeca7b2a6da32cd4c3d6984
CuBeRJAN <59669705+CuBeRJAN@users.noreply.github.com>
2021-11-17 14:05:22 +0100

    Add files via upload

--- i3-status-py.py
+++ i3-status-py.py
@@ -2,10 +2,13 @@ import subprocess
 import time
 import os
 
-statuslen = int(102)
-intray = ["/usr/games/lutris","/home/jan/.local/share/Steam","/usr/share/discord/Discord"]
+statuslen = int(124)
+# 22
+intray = ["lutris","vlc","steam","discord"]
 
 icon_width=3
+
+# do not question how this works, I don't know
 
 def get_date():
     date = subprocess.check_output('date +"%d/%m/%Y | %H:%M"', shell=True)
@@ -46,27 +49,32 @@ def get_volume_mic():
         mute = "[off]"
     return(mute)
 def get_music(symbols,iplen,trayicons,icon_width):
-    try:
+    if 1==1:
         player = str(subprocess.check_output("playerctl -l | head -n 1", shell=True,encoding="utf-8"))
         player_split = player.split(".")
         player = player_split[0]
+        titlestr = "title"
         try:
             artist = str(subprocess.check_output(f"(playerctl -a metadata | grep {player}) | grep artist", shell=True,encoding="utf-8"))
         except:
-            artist = ""
+            artist = str(f"xesam:artist              {player}")
         try:
             title = str(subprocess.check_output(f"(playerctl -a metadata | grep {player}) | grep title", shell=True,encoding="utf-8"))
         except:
-            title=""
-        print(artist,title)
+            try:
+                title = str(subprocess.check_output(f"(playerctl -a metadata | grep {player}) | grep 'xesam:url'", shell=True,encoding="utf-8"))
+                title = title.replace("file://","")
+                title = title.replace("/home/jan","~")
+                titlestr = "xesam:url"
+            except:
+                title = ""
         music=str(title+";;;"+artist)
         music_split = music.split(" ")
 #        if len(music_split) == 3:
         try:
-            print(music)
             music_split = music.split(";;;")
             title = music_split[0]
-            title_split = title.split("title")
+            title_split = title.split(titlestr)
             title = title_split[1]
             artist = music_split[1]
             artist_split = artist.split("artist")
@@ -77,25 +85,46 @@ def get_music(symbols,iplen,trayicons,icon_width):
         except:
             music_split = music.split(";;;")
             title = music_split[0]
-            title_split = title.split("title")
+            title_split = title.split(titlestr)
             title = title_split[1]
-#            title = music_split[0]
-#            title_split = title.split("title")
-#            title = title_split[1]
-#            artist = artist.lstrip()
         title = title.lstrip()
         status = str(subprocess.check_output("playerctl status", shell=True))
         title = title.strip()
         artist = artist.strip()
-#       102=32
-        if symbols > ((statuslen-70)-trayicons):
-            if len(title) > 50:
-                title = title[:47]+"..."
-            if (len(artist)+len(title)) > 50:
-                if len(artist) > 12:
-                    artist = artist[:12]+"..."
-                if len(title) > 29:
-                    title = title[:29]+"..."
+        num = statuslen-102
+        maxlen = 50+num
+        maxlen_title = 47+num
+        maxlen_artist = int(12+(num/2))
+        maxlen_title2 = int(28+(num/2))
+        title_old = title
+        artist_old = artist
+        if symbols > ((statuslen-70-num)-(trayicons*icon_width)):
+            if len(title) > maxlen:
+                title = title[:maxlen_title]+"..."
+            if (len(artist)+len(title)) > maxlen:
+                if len(artist) > maxlen_artist:
+                    artist = artist[:maxlen_artist]+"..."
+                if len(title) > maxlen_title2:
+                    title = title[:maxlen_title2]+"..."
+        if (len(artist)+len(title)+(icon_width*2)+symbols+((trayicons*icon_width)-1)) > statuslen-20:
+            if symbols > ((statuslen-70-num)-(trayicons*icon_width)):
+                if (int((icon_width*trayicons)/2) == (float((icon_width*trayicons)/2))):
+                    odd = False
+                else:
+                    odd = True
+                if len(title_old) > (maxlen-((icon_width)*trayicons)):
+                    if odd:
+                        title = title_old[:maxlen_title-((int((icon_width*trayicons)/2))-1)]+"..."
+                    else:
+                        title = title_old[:maxlen_title-(int((icon_width*trayicons)/2))]+"..."
+                if (len(artist_old)+len(title)) > (maxlen-((icon_width)*trayicons)):
+                    if len(artist) > (maxlen_artist-(int((icon_width/2)*trayicons))):
+                        artist = artist[:(maxlen_artist-int(((icon_width/2)*trayicons)))]+"..."
+                    if len(title) > (maxlen_title2-icon_width):
+                        if odd:
+                            title = title_old[:maxlen_title2-((int((icon_width*trayicons)/2))-1)]+"..."
+                        else:
+                            title = title_old[:maxlen_title2-(int((icon_width*trayicons)/2))]+"..."
         if "Playing" in status:
             if artist != "":
                 playing_str = str(f" {artist} :: {title}")
@@ -106,13 +135,16 @@ def get_music(symbols,iplen,trayicons,icon_width):
                 playing_str = str(f" {artist} :: {title}")
             else:
                 playing_str = str(f" {title}")
-    except: 
+    else: 
         playing_str = ("")
 #   102=83,88   
-    while (len(playing_str)+symbols+iplen) < (statuslen-18-trayicons*icon_width):
+    show_ip = False
+    if trayicons == 0:
+        symbols += 1
+    while (len(playing_str)+symbols+iplen+((trayicons*icon_width)-1)) < (statuslen-20):
         playing_str += " "
         show_ip = True
-    while (len(playing_str)+symbols) < (statuslen-13-trayicons*icon_width) and (show_ip == False):
+    while (len(playing_str)+symbols+((trayicons*icon_width)-1)) < (statuslen-15) and (show_ip == False):
         playing_str += " "
     return(playing_str,show_ip)
 
@@ -120,7 +152,7 @@ while True:
     trayicons=0
     for item in intray:
         try:
-            output = subprocess.check_output(f"ps -AF | grep {item} | grep -v grep", shell=True)
+            output = subprocess.check_output(f"ps -AF | grep -i {item} | grep -v grep", shell=True)
         except:
             output=""
         if output != "":
@@ -139,7 +171,6 @@ while True:
         status = str(f"{status_music} |  {status_volume}  {status_mic} |  {status_date}{space}")
     else:
         status = str(f"{status_music} |  {status_ip} |  {status_volume}  {status_mic} |  {status_date}{space}")
-#    print(status)
     if '"' in status:
         status = status.replace('"',"'")
     os.system(f'xsetroot -name "                                                                                            {status}"')
